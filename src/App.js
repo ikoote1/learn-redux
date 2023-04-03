507a6dd3a7e8964e11bebc77aecd5bcc355d02c5
ikoote1 <ikootepreim@gmail.com>
2023-04-03 14:22:33 +0300

    Setup redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Counter from './Components/Counter';
+import {Counters} from './features/counter/Counters'
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Counters />
     </div>
   );
 }
